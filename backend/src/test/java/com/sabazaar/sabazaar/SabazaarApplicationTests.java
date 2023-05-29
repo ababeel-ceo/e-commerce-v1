f57c92d699d21e5fabd4eb1d76dc77d60f9d8fb2
abdulla <abdullahsmsapk@gmail.com>
2023-05-29 21:33:46 +0530

    initial commit

---
+++ backend/src/test/java/com/sabazaar/sabazaar/SabazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabazaar.sabazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
